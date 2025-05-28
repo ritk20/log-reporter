b67cb5e2393709802646c3a97b900be83b17e915
ritk20 <ritobrotokalita@gmail.com>
2025-05-28 11:30:46 +0530

    correct positioning

---
+++ frontend/src/pages/Public/analytics.tsx
@@ -0,0 +1,9 @@
+export default function AnalyticsPage() {
+  return (
+    <div>
+      <h1>Analytics Dashboard</h1>
+      <p>This is the analytics page, accessible to all authenticated users.</p>
+      {/* Add your analytics components here */}
+    </div>
+  );
+}
