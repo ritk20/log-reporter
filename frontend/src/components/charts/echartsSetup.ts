65e35799d00c098e63c96459581e754998fa5adf
ritk20 <ritobrotokalita@gmail.com>
2025-05-29 13:27:42 +0530

    add pie and crosstabs

---
+++ frontend/src/components/charts/echartsSetup.ts
@@ -0,0 +1,34 @@
+import * as echarts from 'echarts/core'
+import {
+  PieChart,
+  BarChart,
+  ScatterChart,
+  LineChart,
+  GraphChart,
+  HeatmapChart,
+} from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  DatasetComponent,
+  VisualMapComponent,
+} from 'echarts/components'
+import { CanvasRenderer } from 'echarts/renderers'
+
+echarts.use([
+  PieChart,
+  BarChart,
+  ScatterChart,
+  LineChart,
+  GraphChart,
+  HeatmapChart,
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  DatasetComponent,
+  VisualMapComponent,
+  CanvasRenderer,
+])
